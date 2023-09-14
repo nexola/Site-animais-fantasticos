6be43eff30b71dc386df1fbb0e9c2d396062ad68
Vitor Vianna <vitormatheusfv@gmail.com>
2023-09-14 17:01:26 -0300

    Scroll suave adicionado

--- script.js
+++ script.js
@@ -36,6 +36,28 @@ function initAccordion() {
   }
 }
 
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+    console.log(section);
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
 initTabNav();
 
 initAccordion();
+
+initSmoothScroll();
